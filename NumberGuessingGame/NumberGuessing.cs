8085b01a5bd382d4445f83dc72e9d5e33b772530
BarbieSweetDreamz <barbiesweetdreamz@gmail.com>
2026-07-07 14:22:39 +0200

    Created and implemented a number guessing loging and game result enum return type

---
+++ NumberGuessingGame/NumberGuessing.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NumberGuessingGame
+{
+    internal class NumberGuessing
+    {
+         int numberToGuess = 0;
+        
+        void GenerateRandomNumber(int minimum, int maximum)
+        {
+            Random rand = new Random();
+            numberToGuess = rand.Next(minimum, maximum);
+        }
+
+
+    }
+}
