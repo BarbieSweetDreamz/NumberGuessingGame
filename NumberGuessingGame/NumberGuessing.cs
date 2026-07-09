417f0547ddc635f85b7e5fd352218f30cc565420
BarbieSweetDreamz <barbiesweetdreamz@gmail.com>
2026-07-09 10:39:32 +0200

    Forgot to save

--- NumberGuessingGame/NumberGuessing.cs
+++ NumberGuessingGame/NumberGuessing.cs
@@ -1,19 +1,54 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Formats.Tar;
 using System.Text;
 
 namespace NumberGuessingGame
 {
     internal class NumberGuessing
     {
-         int numberToGuess = 0;
-        
-        void GenerateRandomNumber(int minimum, int maximum)
+        private int numberToGuess = 0;
+        private int minimum;
+        private int maximum;
+
+        public NumberGuessing()
+        {
+            GenerateRandomNumber(minimum,maximum);
+
+        }
+       public void GenerateRandomNumber(int minimum, int maximum)
         {
             Random rand = new Random();
             numberToGuess = rand.Next(minimum, maximum);
+
+            this.minimum = minimum;
+            this.maximum = maximum;
         }
 
+       public GuessResult MakeGuess(int guess)
+        {
+            if (guess > maximum )
+            {
+                return GuessResult.OutOflimits;
+            }
+            else if (guess < minimum)
+            {
+                return GuessResult.OutOflimits;
+            }
+            else if (guess == numberToGuess)
+            {
+                GuessResult result = GuessResult.Correct;
+                return result;
+            }
+            else if (guess < numberToGuess)
+            {
+                return GuessResult.TooLow;
+            }
+            else
+            {
+                return GuessResult.TooHigh;
+            }
+        }
 
-    }
+}
 }
